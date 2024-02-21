c34cd5e98b5ea57990177848965a1408fd6ad4e1
Dzulandra Perkasa <dzulandra.perkasa@ui.ac.id>
2024-02-21 20:55:23 +0700

    Edit some scenes

--- scenes/GreenShip.gd
+++ scenes/GreenShip.gd
@@ -1,14 +1,12 @@
 extends RigidBody2D
 
 export var speed = 300.0
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	print("Platform initialized")
-
 var initial_location = Vector2.ZERO
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	print("Platform initialized")
+	initial_location = global_position
+
 func _process(delta):
 	var movement_input = Vector2.ZERO
 	if Input.is_action_pressed("ui_right"):
@@ -24,3 +22,8 @@ func _process(delta):
 
 	var velocity = movement_input * speed
 	set_linear_velocity(velocity)
+
+
+func _on_GreenShip_body_entered(body):
+	if body.is_in_group("Node2D"):
+		global_position = initial_location
